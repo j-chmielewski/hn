baefe55f69bc3540546674e8ede1ba7f58425113
Jacek Chmielewski <jchmielewski@teonite.com>
2021-11-12 15:20:06 +0100

    Fix title selector, add better-panic lib.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use scraper::{Html, Selector};
 static HN_URL: &str = "https://news.ycombinator.com";
 
 fn main() {
+    better_panic::install();
     let content = reqwest::blocking::get(HN_URL)
         .expect("Failed to fetch HN website.")
         .text()
         .unwrap();
     let parsed = Html::parse_document(&content);
     let row_selector = Selector::parse("tr.athing").unwrap();
-    let title_selector = Selector::parse("td.title a.storylink").unwrap();
+    let title_selector = Selector::parse("td.title a.titlelink").unwrap();
     for story in parsed.select(&row_selector) {
         let title = story.select(&title_selector).next().unwrap().inner_html();
         println!("{}", title);
